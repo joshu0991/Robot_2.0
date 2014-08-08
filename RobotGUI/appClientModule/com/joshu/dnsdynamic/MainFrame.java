5517a525da303e9a14d40b03df5560fa27dd5730
josh <joshualilly@me.com>
2014-08-07 23:17:50 -0400

    dded Menubar for new connections to the server

--- RobotGUI/appClientModule/com/joshu/dnsdynamic/MainFrame.java
+++ RobotGUI/appClientModule/com/joshu/dnsdynamic/MainFrame.java
@@ -1,12 +1,23 @@
 package com.joshu.dnsdynamic;
 
-import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
-public class MainFrame extends JFrame{
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+public class MainFrame extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
 	private final int WIDTH = 900;
 	private final int HEIGHT = 600;
+	private JMenuBar menuBar;
+	private JMenu connectMenu;
+	private JMenuItem connectItem;
 	
 	public MainFrame(){
 		super("Robot");
@@ -14,8 +25,23 @@ public class MainFrame extends JFrame{
 		setResizable(false);
 		setSize(WIDTH, HEIGHT);
 		setLocationRelativeTo(null);
+		menuBar = new JMenuBar();
+		connectMenu = new JMenu("Connect");
+		connectItem = new JMenuItem("New Connection", KeyEvent.VK_N);
+		connectItem.addActionListener(this);
+		menuBar.add(connectMenu);
+		connectMenu.add(connectItem);
 		setVisible(true);
+		setJMenuBar(menuBar);
 		add(new MainPanel());
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("New Connection")) {
+		JOptionPane.showInputDialog(this, "Enter the the location");
+		}
 	}
 	
 }
