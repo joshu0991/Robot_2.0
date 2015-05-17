6785aa081d22fc6d0165a40e3997bb9a4d084f11
josh <joshualilly@me.com>
2015-05-17 18:02:59 -0400

    worked on cmake files

--- robot_2.0/trunk/gpioaccess/pinmanager/pinmanager.hpp
+++ robot_2.0/trunk/gpioaccess/pinmanager/pinmanager.hpp
@@ -6,14 +6,15 @@
 /*!
  * \brief class for managing all of the pins in use in the robot applocation is also responsable for cleanup.
  *     All hardware that access GPIO pins should inherit from this class. and add their pins that are intended 
- *     for use to the pinsInUse set.
+ *     for use to the pinsInUse set. Each module may read and write to multiple pins so we will force all modules 
+ *     to implement both functions.
  */
 class PinManager : GPIOPin
 {
 protected:
     //! Possible additions of writing to spi and such so make this pure virtual for flexability.
     virtual bool writeToPin() = 0;   
-    
+    virtual bool readFromPin() = 0;   
 private:
     //! Only want one of these so we can determine which pins are in use and check for conflicts.
     static boost::unordered_set pinsInUse;
---
+++ robot_2.0/trunk/robot/robot.hpp
@@ -0,0 +1,1 @@
+#include "gpioaccess/GPIOPin.hpp"
